195aa12518bfff053185f3e30b490a2c01123306
Yoav Tsoref <yoav@yummi.co.il>
2018-12-30 10:37:47 +0200

    Complete show and create and listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+	def index
+		@articles = Article.all
+	end
 	def new
 		@article = Article.new
 	end
@@ -17,6 +20,30 @@ class ArticlesController < ApplicationController
 
 	def show
 		@article = Article.find(params[:id])
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+		@article = 	Article.find(params[:id])
+		if @article.update(article_params)
+			flash[:notice] = "Article was successfully updated"
+			redirect_to article_path(@article)
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+				@article = 	Article.find(params[:id])
+		if @article.destroy
+			flash[:notice] = "Article was successfully deleted"
+			redirect_to articles_path(@article)
+		else
+			render 'edit'
+		end
 	end
 	private
 		def article_params
