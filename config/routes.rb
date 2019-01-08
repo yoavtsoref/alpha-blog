1b0920f152075302475c9e07b517623dbe84d4ed
Yoav Tsoref <yoav@yummi.co.il>
2019-01-08 12:18:17 +0200

    add users signup

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   root 'pages#home'
 
   get 'about', to: 'pages#about'
+  
+  get 'signup', to: 'users#new'
+
+  resources :users, except: [:new]
 
   resources:articles
 end
